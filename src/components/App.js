3ea91a65c86a97fe7c137fca4f1b03e20beb855e
Ryan Flores <rtflores90@gmail.com>
2019-06-12 11:04:40 -0700

    navigation

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main'
+import Navigation from './Navigation';
 import '../sass/main.scss';
 
 function App() {
   return ( 
     <React.Fragment>
+      <Navigation />
       <Header
         logo={'img/logo-white.png'} 
         headingPrimary={'Outdoors'}
